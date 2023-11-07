c3c38c295c3a1db616e8429e0fc31384d701a33a
Noémie dS <112022872+NoemieDS@users.noreply.github.com>
2023-11-07 17:55:28 -0500

    Diaporama fonctionnel et durée d'affichage sélectionnable dans mode admin par utilisateur

--- diaporama.php
+++ diaporama.php
@@ -2,87 +2,85 @@
 /*
 /*
   * Plugin name: Diaporama 5W5
-  * Description: Cette extension diaporama permet d'afficher les images de la classe Média
+  * Description: Cette extension diaporama permet d'afficher les images de la classe Média et de contrôler certains paramètres d'affichage 
   * Version: 1.0
   * Author: Noémie da Silva, Victor Desjardins, Vincent Gélinas, Vincent Hum, Dac Anne Nguyen
   * Author URI: https://github.com/5W5-Equipe-2
   */
 
+function mon_enqueue_dia_css_js()
+{
+  $version_css = filemtime(plugin_dir_path(__FILE__) . "style.css");
+  $version_js = filemtime(plugin_dir_path(__FILE__) . "js/diaporama.js");
 
-   
-  function mon_enqueue_css_js() {
-    $version_css = filemtime(plugin_dir_path( __FILE__ ) . "style.css");
-     $version_js = filemtime(plugin_dir_path(__FILE__) . "js/diaporama.js");
- 
-     wp_enqueue_style(   '5w5_plugin_diaporama_css',
-     plugin_dir_url(__FILE__) . "style.css",
-     array(),
-     $version_css);
- 
-    wp_enqueue_script(  '5w5_plugin_diaporama_js',
-    plugin_dir_url(__FILE__) ."js/diaporama.js",
+  wp_enqueue_style(
+    '5w5_plugin_diaporama_css',
+    plugin_dir_url(__FILE__) . "style.css",
+    array(),
+    $version_css
+  );
+
+  wp_enqueue_script(
+    '5w5_plugin_diaporama_js',
+    plugin_dir_url(__FILE__) . "js/diaporama.js",
     array(),
     $version_js,
-    true); //permet d'ajouter le JS à la fin de la page
-   }
- 
-   add_action('wp_enqueue_scripts', 'mon_enqueue_css_js');
+    true
+  ); 
 
-  /******************************************************************************** 
-  * MODE ADMINISTRATEUR
-  */
-  function mon_diaporama_settings_page() {
-    add_menu_page('Diaporama Settings', 'Diaporama', 'manage_options', 'mon-diaporama-settings', 'mon_diaporama_settings_page_content');
+  wp_localize_script('5w5_plugin_diaporama_js', 'diaporama_settings', array(
+    'interval_duration' => get_option('diaporama_interval_duration', 3000)
+  ));
+}
+
+add_action('wp_enqueue_scripts', 'mon_enqueue_dia_css_js');
+
+/******************************************************************************** 
+ * MODE ADMINISTRATEUR
+ */
+function mon_diaporama_settings_page()
+{
+  add_menu_page('Diaporama Settings', 'Diaporama', 'manage_options', 'mon-diaporama-settings', 'mon_diaporama_settings_page_content');
 }
 
 
-function mon_diaporama_settings_page_content() {
+function mon_diaporama_settings_page_content()
+{
   // Vérifiez les autorisations de l'utilisateur
   if (!current_user_can('manage_options')) {
-      return;
+    return;
   }
 
   // Enregistrez les paramètres si le formulaire est soumis
   if (isset($_POST['mon_diaporama_submit'])) {
-      update_option('mon_diaporama_theme', $_POST['mon_diaporama_theme']);
-      echo '<div class="updated"><p>Thème mis à jour.</p></div>';
+    // Vérifiez si le formulaire a été soumis
+    $interval_duration = intval($_POST['interval-duration']);
+    update_option('diaporama_interval_duration', $interval_duration); // Enregistrez la durée dans les options de l'extension
   }
 
   // Affichez le formulaire de configuration du diaporama
-  $diaporama_theme = get_option('mon_diaporama_theme', 'light');
-  ?>
-  <div class="wrap">
-      <h2>Paramètrez le diaporama</h2>
-      <form method="post" action="">
-        <h3>Choississez l'alignement des images</h3>
-          <label for="mon_diaporama_theme">Alignement latéral (x) :</label>
-          <select name="mon_diaporama_theme" id="mon_diaporama_theme">
-              <option value="left" <?php selected($diaporama_theme, 'left'); ?>>Gauche</option>
-              <option value="50" <?php selected($diaporama_theme, '50'); ?>>Centre</option>
-              <option value="right" <?php selected($diaporama_theme, 'right'); ?>>Droite</option>
-          </select>
-          <label for="mon_diaporama_theme">Alignement vertical (y) :</label>
-          <select name="mon_diaporama_theme" id="mon_diaporama_theme">
-              <option value="top" <?php selected($diaporama_theme, 'top'); ?>>Haut</option>
-              <option value="50" <?php selected($diaporama_theme, '50'); ?>>Centre</option>
-              <option value="bottom" <?php selected($diaporama_theme, 'bottom'); ?>>Bas</option>
-          </select>
-          <input type="submit" name="mon_diaporama_submit" class="button-primary" value="Enregistrer">
-      </form>
+  $diaporama_theme = get_option('mon_diaporama_theme');
+?>
+
+  <h2>Paramètrez le diaporama</h2>
+  <div>
+    <h3>Choississez la durée d'affichage des images</h3>
+    <h4>La durée actuelle d'affichage des images est de <?php echo isset($interval_duration) ? $interval_duration : 3000; ?> ms.</h4>
+    <form id="diaporama-settings-form" method="post">
+      <label for="interval-duration">Nouvelle durée, en millisecondes :</label>
+      <input type="number" id="interval-duration" name="interval-duration" min="100" />
+      
+      <input type="submit" name="mon_diaporama_submit" value="Enregistrer" />
+    </form>
   </div>
-  <?php
+
+
+<?php
+  // Récupérez la durée enregistrée dans les options de l'extension
+  $saved_duration = get_option('diaporama_interval_duration', 3000);
+
+  // Ajoutez la durée dans une balise script
+  echo "<script>var savedDuration = $saved_duration;</script>";
 }
-
 add_action('admin_menu', 'mon_diaporama_settings_page');
-
-
-  /******************************************************************************** 
-  * EXTENSION
-  */
-
-
-
-
-
-  ?>
-
+?>
--- js/diaporama.js
+++ js/diaporama.js
@@ -1,11 +1,45 @@
 (function () {
+  let diaporama = document.querySelector(".diaporama");
+  let images = diaporama.querySelectorAll("img");
+  let idx = 0;
+  // Durée de l'intervalle par défaut (en millisecondes)
+  let duration = diaporama_settings.interval_duration || 3000; // Durée de l'intervalle par défaut (en millisecondes)
+  let diaporamaInterval; // Variable pour stocker l'intervalle
 
-let diaporama = document.querySelector('.diaporama');
+  /* On applique la classe CSS pour l'effet "Moody" */
+  diaporama.classList.add("img-wrapper");
 
-console.log("Hello world!");
-console.log(diaporama);
+  //console.log("durée",duration);
 
-diaporama.classList.add("img-wrapper");
+  // Fonction pour afficher l'image suivante et faire rouler le diaporama
+  function afficherImageSuivante() {
+    // Masquez l'image actuelle avec animation
+    images[idx].classList.remove("fondu");
+    images[idx].style.display = "none";
 
+    idx = (idx + 1) % images.length; // Passez à l'image suivante (ou revenez à la première si nécessaire)
 
+    // Affichez l'image suivante avec animation
+    images[idx].style.display = "flex";
+    images[idx].classList.add("fondu");
+  }
+
+  // Fonction pour mettre à jour la durée de l'intervalle
+  function updateIntervalDuration(newDuration) {
+    duration = newDuration;
+    clearInterval(diaporamaInterval); // Effacer l'intervalle précédent
+    diaporamaInterval = setInterval(afficherImageSuivante, duration); // Créer un nouvel intervalle avec la nouvelle durée
+  }
+
+  // Démarrez un minuterie pour changer automatiquement d'image à intervalles réguliers
+  diaporamaInterval = setInterval(afficherImageSuivante, duration);
+
+  // Recherchez un élément qui permettra à l'utilisateur de régler la durée
+  const durationInput = document.getElementById("interval-duration");
+  if (durationInput) {
+    durationInput.addEventListener("change", function () {
+      const newDuration = parseInt(durationInput.value);
+      updateIntervalDuration(newDuration);
+    });
+  }
 })();
