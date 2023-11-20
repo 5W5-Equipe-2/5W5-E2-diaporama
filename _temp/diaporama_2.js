251c3354a8b0d04763f78250c4ffc58c112b8ce3
Noémie dS <112022872+NoemieDS@users.noreply.github.com>
2023-11-20 07:14:38 -0500

    Retour à la version précédente

--- _temp/diaporama_2.js
+++ _temp/diaporama_2.js
@@ -1,7 +1,7 @@
 (function () {
   let diaporama = document.querySelector(".diaporama");
   let images = diaporama.querySelectorAll("img");
-  let idx = 0;
+  let idx = Math.floor(Math.random() * images.length);
   let diaporamaInterval; // Variable pour stocker l'instance du setIntervalle
 
   //Les variables modifiables dans le tableau de bord de l'extension, dans WP
@@ -10,18 +10,27 @@
 
   console.log("desaturation", desaturation);
   console.log("durée", duree);
+  console.log("idx", idx);
 
-  /* On enlève la classe CSS pour masquer l'image (avant que le JS s'applique) */
-  diaporama.classList.remove("masquer-image");
+  const afficherImage = (idx) => {
+    diaporama.classList.remove("masquer-image");
+    /* On applique les classes CSS pour les effets sur les images */
+    diaporama.classList.add("img-wrapper");
+    // On appliquez le filtre de desaturation des images avec la valeur choisit par l'utilisateur
+    diaporama.style.filter = `grayscale(${desaturation}%)`;
 
-  /* On applique les classes CSS pour les effets sur les images */
-  diaporama.classList.add("img-wrapper");
- 
-  // On appliquez le filtre de desaturation des images avec la valeur choisit par l'utilisateur
-  diaporama.style.filter = `grayscale(${desaturation}%)`;
+    // Affichez l'image spécifique à l'index passé en paramètre
+    images[idx].style.display = "flex";
 
-  // Démarrez un minuterie pour changer automatiquement d'image à intervalles réguliers
+     // Démarrez un minuterie pour changer automatiquement d'image à intervalles réguliers
   diaporamaInterval = setInterval(afficherImageSuivante, duree);
+};
+
+afficherImage(idx); 
+
+
+ // Démarrez un minuterie pour changer automatiquement d'image à intervalles réguliers
+ diaporamaInterval = setInterval(afficherImageSuivante, duree);
 
   // Fonction pour afficher l'image suivante et faire rouler le diaporama
   function afficherImageSuivante() {
